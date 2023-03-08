e92d52e2c1b7dfe99a31f1c5c1ed80af2a77bc0f
annaantonn <105316891+annaantonn@users.noreply.github.com>
2023-03-07 16:54:29 -0800

    Add files via upload

---
+++ Assingment1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+9+9
+
+
+# In[3]:
+
+
+9-1
+
+
+# In[4]:
+
+
+9*2
+
+
+# ![Waterfalls are beautiful!](images/mult_falls_2.jpg "Multnomah Falls")
+
+# ![Waterfalls are beautiful!](https://www.travelportland.com/wp-content/uploads/2019/08/cropped-NR_Nearby_ColumbiaGorge_MultnomahFalls_99837764_shutterstock_tusharkoley-.jpg)
+# 
+# <em>Multnomah Falls</em> is a **beautiful** waterfall in Oregon. It is the largest waterfall in Oregon. 
+# 
+# 
+
+# <h1>Assignment1</h1>
+
+# <h2>Anna Anton github username: annaantonn</h2>
+
+# <h3>January 12, 2023</h3>
+
+# In[ ]:
+
+
+
+
